731eb335e4a297249ddd35d3e9f4ea17fa27f1df
Marisabel Trejo <marisabeltrejoo@gmail.com>
2022-03-15 16:24:12 -0600

    changes

--- login.js
+++ login.js
@@ -1,64 +1,86 @@
-import React from 'react';
-import {StyleSheet, View, Text, Button} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, Button, SafeAreaView, TextInput} from 'react-native';
+
 
 export default function Login(props){
-    return(
-        <View style={styles.login}>
-            <Text>This is the Login Screen</Text>
-            <Button title="Log In" onPress={()=>props.setUserLoggedIn(true)}></Button>
-        </View>
+  const [phoneNumber, setPhoneNumber] = React.useState("Enter Phone Number:");
+  const [OneTimePassword, set_otp] = React.useState(null); 
+  //change 
+  
+  return (
+    <SafeAreaView>
+      <TextInput
+        style={styles.input} //login
+        placeholder="Phone Number"
+        onChangeText={setPhoneNumber}
+        value={phoneNumber}
+        keyboardType = "numeric"
+        
+      />
 
-    );
+      <Button title="Send Text" onPress={()=>send_sms(phoneNumber)(true)}/>
+
+
+      <TextInput
+        style={styles.input} //login
+        onChangeText={set_otp}
+        placeholder="one time code"
+        keyboardType="numeric"
+        value={OneTimePassword}
+      />
+      <Button title="Log In" onPress={()=>props.setUserLoggedIn(true)}
+      />
+    </SafeAreaView>
+  );
 }
 
-const styles = StyleSheet.create({
-    login: {
-        flexDirection: 'row',
-        width: '100%',
-        justifyContent: 'space-between',
-        backgroundColor: 'green',
-        height: '12%',
-        alignItems: 'flex-end',
-        paddingBottom: 5,
-        paddingLeft: 10,
-        paddingRight: 10,
-        
-      },
-})
-// import React from "react";
-// import { SafeAreaView, StyleSheet, TextInput } from "react-native";
 
+const send_sms = async (phoneNumber) => {
+  await fetch('https://dev.stedi.me/twofactorlogin/', + phoneNumber, {
+  method: 'POST',
 
-// const UselessTextInput = () => {
-//   const [text, onChangeText] = React.useState("Phone Number");
-//   const [number, onChangeNumber] = React.useState(null);
-  
+  body: JSON.stringify({
+    firstParam: 'phoneNumber',
+    secondParam: 'OneTimePassword'
+  })
+});
 
-//   return (
-//     <SafeAreaView>
-//       <TextInput
-//         style={styles.input}
-//         onChangeText={onChangeText}
-//         value={text}
-//       />
-//       <TextInput
-//         style={styles.input}
-//         onChangeText={onChangeNumber}
-//         value={number}
-//         placeholder="one time code"
-//         keyboardType="numeric"
-//       />
-//     </SafeAreaView>
-//   );
-// };
+const set_otp = async (OneTimePassword, phoneNumber)  => 
+  await fetch('https://dev.stedi.me/twofactorlogin/', {
+  method: 'POST',
+  headers: {
+    Accept: 'application/text', //json
+    'Content-Type': 'application/text'///json
+  },
+  body: JSON.stringify({
+    phoneNumber,
+  OneTimePassword
+  })
+});
+}
 
 // const styles = StyleSheet.create({
-//   input: {
-//     height: 40,
-//     margin: 12,
-//     borderWidth: 1,
-//     padding: 10,
-//   },
+//     login: {
+//         flexDirection: 'row',
+//         width: '100%',
+//         justifyContent: 'space-between',
+//         backgroundColor: 'green',
+//         height: '12%',
+//         alignItems: 'flex-end',
+//         paddingBottom: 5,
+//         paddingLeft: 10,
+//         paddingRight: 10,
+        
+//       },
 // });
 
-// export default UselessTextInput;
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+
